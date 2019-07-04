934603e4aca268ef5066840ab888c28e580bc193
dgchanda <dgchanda@DIN73002068.corp.capgemini.com>
2019-07-04 17:39:28 +0530

    add

--- project/src/main/java/com/cg/sample/Addition.java
+++ project/src/main/java/com/cg/sample/Addition.java
@@ -2,6 +2,6 @@ package com.cg.sample;
 
 public class Addition {
    public static void main(String args[]) {
-	   System.out.println("addition of two nos");
+	   System.out.println("addition of two os");
    }
 }
