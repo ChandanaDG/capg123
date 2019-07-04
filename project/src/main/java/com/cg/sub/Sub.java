78507b17404d94a2f7dec14b9bd713e871f50973
dgchanda <dgchanda@DIN73002068.corp.capgemini.com>
2019-07-04 18:35:25 +0530

    Diff commited

---
+++ project/src/main/java/com/cg/sub/Sub.java
@@ -0,0 +1,24 @@
+package com.cg.sub;
+
+public class Sub 
+{
+	private int e,d;
+
+	public int getE() {
+		return e;
+	}
+
+	public void setE(int e) {
+		this.e = e;
+	}
+
+	public int getD() {
+		return d;
+	}
+
+	public void setD(int d) {
+		this.d = d;
+	}
+	
+
+}
